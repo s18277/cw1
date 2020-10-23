c418613213dd75575833beed01fe4ea912a0a4fb
Paweł <73314096+s18277@users.noreply.github.com>
2020-10-23 20:21:28 +0200

    Add simple version of HTTP e-mail crawler.

---
+++ Cw1/HttpEmailCrawler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cw1 {
+
+    internal static class HttpEmailCrawler {
+
+        private const string EmailPattern = "[\\w-.]+@[\\w-.]+";
+
+        static void Main(string[] args) {
+            foreach (var url in args) {
+                ShowAllEmailsInUrl(url);
+            }
+        }
+
+        private static void ShowAllEmailsInUrl(string url) {
+            Console.WriteLine($"Looking for email addresses in {url}:");
+            var httpGetResponse = GetResponseFromUrl(url).Result;
+            var emails = GetEmailsFromResponse(httpGetResponse);
+            foreach (var email in emails) {
+                Console.WriteLine(email);
+            }
+            Console.WriteLine();
+        }
+
+        private static async Task<string> GetResponseFromUrl(string url) {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(url);
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        private static MatchCollection GetEmailsFromResponse(string httpGetResponse) {
+            var emailRegex = new Regex(EmailPattern);
+            return emailRegex.Matches(httpGetResponse);
+        }
+
+    }
+
+}
--- Cw1/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Cw1 {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
